4e1dd32ce6f6b9fc3e42509a197aa99dff278148
Guguskis <matasliutikas@gmail.com>
2022-02-24 16:39:05 +0200

    init commit

---
+++ src/main/java/lt/liutikas/reddit/RedditScrapperApplication.java
@@ -0,0 +1,13 @@
+package lt.liutikas.reddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditScrapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedditScrapperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lt/liutikas/reddit/RedditScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.liutikas.reddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditScrapperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
