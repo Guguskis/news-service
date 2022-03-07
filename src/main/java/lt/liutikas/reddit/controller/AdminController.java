a70683fd745bd313042dad084960c56eebb96c08
Guguskis <matasliutikas@gmail.com>
2022-03-07 20:22:00 +0200

    cleanup admin endpoint

--- src/main/java/lt/liutikas/reddit/controller/AdminController.java
+++ src/main/java/lt/liutikas/reddit/controller/AdminController.java
@@ -2,27 +2,28 @@ package lt.liutikas.reddit.controller;
 
 import lt.liutikas.reddit.model.NewsEvent;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
 
-    private final SimpMessagingTemplate pushTemplate;
     private final ApplicationEventPublisher eventPublisher;
 
-    public AdminController(SimpMessagingTemplate pushTemplate, ApplicationEventPublisher eventPublisher) {
-        this.pushTemplate = pushTemplate;
+    public AdminController(ApplicationEventPublisher eventPublisher) {
         this.eventPublisher = eventPublisher;
     }
 
     @PostMapping("/news/publish")
-    public void sendMessage(String headline) {
+    public void sendMessage(String headline) throws MalformedURLException {
         NewsEvent event = new NewsEvent(this);
         event.setHeadline(headline);
+        event.setUrl(new URL("https://www.reddit.com/r/worldnews/comments/7xqzqy/trump_says_he_will_not_be_president_for_next/"));
         eventPublisher.publishEvent(event);
     }
 
