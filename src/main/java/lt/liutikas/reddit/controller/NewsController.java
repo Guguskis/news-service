b39fefe61cb39aebfc4276d345bfcb4fe16b7a1e
Guguskis <matasliutikas@gmail.com>
2022-03-22 22:56:23 +0200

    moved logic to another class

--- src/main/java/lt/liutikas/reddit/controller/NewsController.java
+++ src/main/java/lt/liutikas/reddit/controller/NewsController.java
@@ -1,11 +1,8 @@
 package lt.liutikas.reddit.controller;
 
 import lt.liutikas.reddit.model.NewsPage;
-import lt.liutikas.reddit.model.NewsSubscriptionMessage;
 import lt.liutikas.reddit.model.PaginationQuery;
 import lt.liutikas.reddit.service.NewsService;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,13 +22,6 @@ public class NewsController {
     @GetMapping
     public NewsPage getAll(@Valid PaginationQuery query) {
         return newsService.getAll(query.pageRequest());
-    }
-
-    @MessageMapping("/news/reddit")
-//    @SendToUser("/queue/news/reddit")
-    public void handleNewsSubscription(NewsSubscriptionMessage message,
-                                       @Header("simpSessionId") String sessionId) {
-        newsService.processNewsSubscription(sessionId, message);
     }
 
 }
