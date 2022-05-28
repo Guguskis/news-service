5719041e8200ec113a13e92408ded5e72d5f191e
Guguskis <matasliutikas@gmail.com>
2022-05-29 02:42:41 +0300

    remove api annotations

--- src/main/java/lt/liutikas/reddit/model/api/GetNewsRequest.java
+++ src/main/java/lt/liutikas/reddit/model/api/GetNewsRequest.java
@@ -1,13 +1,10 @@
 package lt.liutikas.reddit.model.api;
 
-import io.swagger.annotations.ApiModelProperty;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class GetNewsRequest extends PaginationQuery {
 
-    @ApiModelProperty(value = "List of subChannels to get news from", required = true, example = "funny,pics")
     public List<String> subChannels = new ArrayList<>();
 
     public List<String> getSubChannels() {
--- src/main/java/lt/liutikas/reddit/model/api/PaginationQuery.java
+++ src/main/java/lt/liutikas/reddit/model/api/PaginationQuery.java
@@ -1,6 +1,5 @@
 package lt.liutikas.reddit.model.api;
 
-import io.swagger.annotations.ApiModelProperty;
 import org.springframework.data.domain.PageRequest;
 
 import javax.validation.constraints.Min;
@@ -10,7 +9,6 @@ public class PaginationQuery {
     @Min(0)
     private Integer pageToken = 0;
     @Min(1)
-    @ApiModelProperty(value = "Number of items per page", example = "10")
     private Integer pageSize = 10;
 
     public PageRequest pageRequest() {
