4a1e933ff0c606a812db8b4c06b00668b7b04b5d
Guguskis <matasliutikas@gmail.com>
2022-05-31 00:56:04 +0300

    revert websocket API extraction

--- src/main/java/lt/liutikas/reddit/api/controller/NewsWebsocketController.java
+++ src/main/java/lt/liutikas/reddit/api/controller/NewsWebsocketController.java
@@ -1,10 +1,7 @@
 package lt.liutikas.reddit.api.controller;
 
 import lt.liutikas.reddit.api.model.NewsSubscription;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.MessageMapping;
 
 public interface NewsWebsocketController {
-    @MessageMapping("/queue/news")
-    void handleNewsSubscription(NewsSubscription message, @Header("simpSessionId") String sessionId);
+    void handleNewsSubscription(NewsSubscription message, String sessionId);
 }
--- src/main/java/lt/liutikas/reddit/controller/DefaultNewsWebsocketController.java
+++ src/main/java/lt/liutikas/reddit/controller/DefaultNewsWebsocketController.java
@@ -3,6 +3,8 @@ package lt.liutikas.reddit.controller;
 import lt.liutikas.reddit.api.controller.NewsWebsocketController;
 import lt.liutikas.reddit.api.model.NewsSubscription;
 import lt.liutikas.reddit.service.NewsService;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +16,8 @@ public class DefaultNewsWebsocketController implements NewsWebsocketController {
     }
 
     @Override
-    public void handleNewsSubscription(NewsSubscription message, String sessionId) {
+    @MessageMapping("/queue/news")
+    public void handleNewsSubscription(NewsSubscription message, @Header("simpSessionId") String sessionId) {
         newsService.processNewsSubscription(sessionId, message);
     }
 }
