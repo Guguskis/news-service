a2b9799caa30e22396f2ebbea87f3baaf0605b83
Guguskis <matasliutikas@gmail.com>
2022-06-04 21:57:19 +0300

    fix admin controller path

--- src/main/java/lt/liutikas/reddit/api/controller/AdminController.java
+++ src/main/java/lt/liutikas/reddit/api/controller/AdminController.java
@@ -3,9 +3,11 @@ package lt.liutikas.reddit.api.controller;
 import lt.liutikas.reddit.model.core.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequestMapping("/api/admin")
 public interface AdminController {
     @GetMapping("/users")
     List<User> getUsers();
